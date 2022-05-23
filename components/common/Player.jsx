8c4c0e7149a0bd6b1dbd2389ba102c23f3ed93dc
ulysses316 <ulises316@live.com.mx>
2022-05-23 18:56:10 -0500

    [ADD] Player section and grid

--- components/common/Player.jsx
+++ components/common/Player.jsx
@@ -2,6 +2,16 @@ import styles from '../../styles/components/common/Player.module.scss'
 
 export default function Player() {
   return (
-    <div className={styles["player"]}></div>
+    <div className={styles["player"]}>
+      <div className={styles["player-album-info"]}>
+        <p>a</p>
+      </div>
+      <div className={styles["player-controls"]}>
+        <p>b</p>
+      </div>
+      <div className={styles["player-options"]}>
+        <p>c</p>  
+      </div>      
+    </div>
   )
 }
